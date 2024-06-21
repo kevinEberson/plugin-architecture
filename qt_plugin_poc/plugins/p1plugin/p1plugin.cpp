ef98a7b98df7ae289dbd403fe79a6c51e28d891d
Kevin <459792@fhict.nl>
2024-06-21 16:01:21 +0200

    renamed goodbyworldplugin to p1plugin

---
+++ qt_plugin_poc/plugins/p1plugin/p1plugin.cpp
@@ -0,0 +1,7 @@
+#include "p1plugin.h"
+#include <QDebug>
+
+void P1Plugin::usePlugin()
+{
+    qDebug() << "Goodby World!";
+}
---
+++ qt_plugin_poc/plugins/p1plugin/p1plugin.h
@@ -0,0 +1,17 @@
+#ifndef P1PLUGIN_H
+#define P1PLUGIN_H
+
+#include <QObject>
+#include "plugininterface.h"
+
+class P1Plugin : public QObject, public PluginInterface
+{
+    Q_OBJECT
+    Q_INTERFACES(PluginInterface)
+    Q_PLUGIN_METADATA(IID "org.example.PluginInterface" FILE "plugininterface.json")
+
+public:
+    void goodby() override;
+};
+
+#endif // P1PLUGIN_H
