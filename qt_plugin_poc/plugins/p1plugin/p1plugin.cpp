7a31804d0aef2821df48d8c1407d27f7766ac1fa
Kevin <459792@fhict.nl>
2024-06-24 14:53:07 +0200

    Added Get request method to get fake data from the broker

--- qt_plugin_poc/plugins/p1plugin/p1plugin.cpp
+++ qt_plugin_poc/plugins/p1plugin/p1plugin.cpp
@@ -1,7 +1,34 @@
 #include "p1plugin.h"
-#include <QDebug>
+#include <QNetworkAccessManager>
+#include <QNetworkRequest>
+#include <QNetworkReply>
+#include <QEventLoop>
+#include <QUrl>
 
 void P1Plugin::usePlugin()
 {
-    qDebug() << "Getting data from broker";
+    // Create a QNetworkAccessManager
+    QNetworkAccessManager manager;
+
+    // Create a request for the endpoint
+    QNetworkRequest request(QUrl("http://localhost:1880/power_delivered"));
+
+    // Make a synchronous GET request
+    QEventLoop loop;
+    QNetworkReply *reply = manager.get(request);
+    QObject::connect(reply, &QNetworkReply::finished, &loop, &QEventLoop::quit);
+    loop.exec();
+
+    // Check for errors
+    if (reply->error() == QNetworkReply::NoError) {
+        // Read the response as a string
+        QString response = reply->readAll();
+        qDebug() << "Response: " << response;
+    } else {
+        // Handle errors
+        qDebug() << "Error: " << reply->errorString();
+    }
+
+    // Clean up
+    reply->deleteLater();
 }
